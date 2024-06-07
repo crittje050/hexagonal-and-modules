27a4c7765486ad1e12b0e9759ff17c5d4e17fb46
christianmartin <christian.martin@ordina.nl>
2024-06-07 12:34:10 +0200

    Init commit containing the modules structure

---
+++ infrastructure/db-adapter/src/main/java/org/example/dbadapter/DbAdapterApplication.java
@@ -0,0 +1,13 @@
+package org.example.dbadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbAdapterApplication.class, args);
+    }
+
+}
---
+++ infrastructure/db-adapter/src/test/java/org/example/dbadapter/DbAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dbadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
